c883b85b64636a7fb55793306a0a3fae55db93fe
FrantaSrc <165063870+FrantaSrc@users.noreply.github.com>
2024-05-03 15:08:49 +0200

    Add files via upload
    Signed-off-by: FrantaSrc <165063870+FrantaSrc@users.noreply.github.com>

---
+++ Maps/Sets/Arbol1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Arbol1" tilewidth="64" tileheight="64" tilecount="32" columns="4">
+ <image source="Arbol1.png" width="256" height="512"/>
+</tileset>
