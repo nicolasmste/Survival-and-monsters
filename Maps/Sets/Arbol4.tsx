57af158ebf5784b67aa5eb5b799620b988cbd39c
Nicolas <143967233+nicolasmste@users.noreply.github.com>
2024-05-05 15:48:52 +0200

    Delete Maps directory
    Signed-off-by: Nicolas <143967233+nicolasmste@users.noreply.github.com>

--- Maps/Sets/Arbol4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Arbol4" tilewidth="64" tileheight="64" tilecount="64" columns="8">
- <image source="Arbol4.png" width="512" height="512"/>
-</tileset>
