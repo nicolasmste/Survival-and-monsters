d1c547f9f787834cbaa0ecc97e1e4b9379d8e545
Nicolas <143967233+nicolasmste@users.noreply.github.com>
2024-05-05 15:50:27 +0200

    Delete Survival And Monsters directory
    Signed-off-by: Nicolas <143967233+nicolasmste@users.noreply.github.com>

--- Survival And Monsters/main/Attacks.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class Attack(pygame.sprite.Sprite):
-
-    def __init__(self,x,y):
-        super().__init__()
-        
--- Survival And Monsters/main/main.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from game import *
-
-
-if __name__ == '__main__':
-    pygame.init()   #initialisation du jeu
-    game = Play()
-    game.run()
-
